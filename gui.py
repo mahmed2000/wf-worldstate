300ddb523a28f9febd4d79896df66fb73d2504d1
Muaz Ahmad <m.ahmed_2000@hotmail.com>
2022-11-08 15:52:05 +0500

    Basic setup to run template

---
+++ gui.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+import sys
+import gui_base
+
+class ExampleApp(QtWidgets.QMainWindow, gui_base.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    gui = ExampleApp()
+    gui.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
+    
