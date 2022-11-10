b00080d5ed55fcf8ad657d7eed2f648c654642cf
Muaz Ahmad <m.ahmed_2000@hotmail.com>
2022-11-10 16:25:46 +0500

    Added api functionality

---
+++ api.py
@@ -0,0 +1,26 @@
+import requests, json
+
+class WorldState():
+    def __init__(self):
+        self.client = requests.Session()
+        self.ws = {}
+        self.get_ws()
+
+    def get_ws(self):
+        try:
+            self.ws = json.loads(self.client.get('https://api.warframestat.us/pc?language=en').text)
+        except requests.exceptions.ConnectionError:
+            return False
+
+        self.fissures = self.ws.get('fissures')
+        self.invasions = self.ws.get('invasions')
+        self.sorties = self.ws.get('sorties')
+        self.archon_hunt = self.ws.get('archonHunt')
+        self.arbitration = self.ws.get('arbitration')
+        self.alerts = self.ws.get('alerts')
+        self.baro_items = self.ws.get('voidTrader').get('inventory')
+        self.nightwave = self.ws.get('nightwave')
+        self.events = self.ws.get('events')
+        self.news = self.ws.get('news')
+
+        return True
